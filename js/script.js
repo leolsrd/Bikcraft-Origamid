9c299d8b24fc90bee80159a8688686fdbcb40441
Léo Lourenço <leo.lsrd@gmail.com>
2024-04-09 23:07:35 -0300

    feat: Finalizado a aula plugin animar

--- js/script.js
+++ js/script.js
@@ -44,3 +44,30 @@ function eventosPerguntas(pergunta) {
 }
 
 perguntas.forEach(eventosPerguntas);
+
+// Galeria de Bicicletas
+
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventoGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventoGaleria);
+
+// Animação do Plugin
+
+// Verificando se o arquivo SimpleAnime foi carregado e se foi executa o mesmo
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
