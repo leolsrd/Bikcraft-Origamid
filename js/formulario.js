924d747d60e07b1f3ff0d729eef8c5be5ba2c7e6
Léo Lourenço <leo.lsrd@gmail.com>
2024-04-13 13:37:38 -0300

    feat: finalizado o último modeulo do curso HTML, CSS, JS

---
+++ js/formulario.js
@@ -0,0 +1,27 @@
+const formulario = document.querySelector("form");
+
+function formularioEnviado(resposta) {
+  if (resposta.ok) {
+    formulario.innerHTML =
+      "<p class='font-2-l' style='grid-column: 1/-1; padding: 1rem; border-radius: 4px; background: #f7f7f7;'><span style='color: #317100';>Mensagem enviada</span>  entraremos em contato em breve. Geralmetne em 24 horas.</p>";
+  } else {
+    formulario.innerHTML =
+      "<p class='font-2-l' style='grid-column: 1/-1; padding: 1rem; border-radius: 4px; background: #f7f7f7;'><span style='color: #e00000';>Erro no envio,</span> este site é apenas para estudo</p>";
+  }
+}
+
+function enviarFormulario(event) {
+  event.preventDefault();
+  const botao = document.querySelector("form button");
+  botao.disabled = true;
+  botao.innerText = "Enviando...";
+
+  const data = new FormData(formulario);
+
+  fetch("./enviar.php", {
+    method: "POST",
+    body: data,
+  }).then(formularioEnviado);
+}
+
+formulario.addEventListener("submit", enviarFormulario);
